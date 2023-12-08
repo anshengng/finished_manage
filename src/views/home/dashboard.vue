4fca858bb5f49cee452fb07698497468d1cb25ec
LiuKun <13655914562@163.com>
2023-12-08 23:45:37 +0800

    Update dashboard.vue

--- src/views/home/dashboard.vue
+++ src/views/home/dashboard.vue
@@ -1,11 +1,27 @@
 <script setup lang="ts">
-
+const cardList = [
+    { title: 'car1', content: 'content1' },
+    { title: 'car2', content: 'content2' },
+    { title: 'car3', content: 'content3' },
+    { title: 'car4', content: 'content4' }
+] 
 </script>
 
 
 <template>
-    <div class="bg-white rounded-xl overflow-y-auto">
-        dashboard
+    <div class="bg-white rounded-xl overflow-y-auto grid grid-cols-4 gap-2 ">
+        <el-card shadow="hover" v-for="(card, index) in cardList" :key="index" :body-style="{ padding: '20px' }">
+            <template #header>
+                <div>
+                    <span>{{ card.title }}</span>
+                </div>
+            </template>
+            <!-- card body -->
+            <div>
+                {{ card.content }}
+            </div>
+        </el-card>
+
     </div>
 </template>
 

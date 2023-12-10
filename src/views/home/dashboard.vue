be0d99feb82cd17127bdd58164d1bce69930f3a0
LiuKun <13655914562@163.com>
2023-12-10 22:18:32 +0800

    Update dashboard.vue

--- src/views/home/dashboard.vue
+++ src/views/home/dashboard.vue
@@ -9,21 +9,21 @@ const cardList = [
 
 
 <template>
-    <div class="bg-white rounded-xl overflow-y-auto grid grid-cols-4 gap-2 ">
-        <el-card shadow="hover" v-for="(card, index) in cardList" :key="index" :body-style="{ padding: '20px' }">
+    <div class="bg-white rounded-xl overflow-y-auto grid grid-cols-4 gap-2 py-1 pr-2">
+        <el-card shadow="hover" v-for="(card, index) in cardList" :key="index" :body-style="{ padding: '20px' }" class="cursor-pointer">
             <template #header>
                 <div>
                     <span>{{ card.title }}</span>
                 </div>
             </template>
             <!-- card body -->
-            <div>
+            <div class="min-h-[150px]">
                 {{ card.content }}
             </div>
         </el-card>
-
     </div>
 </template>
 
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+</style>

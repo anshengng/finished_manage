5d7b3803f6b00fa3c67e29857230b128bf30cc0c
LiuKun <13655914562@163.com>
2023-12-11 22:27:53 +0800

    ud all_frame

--- src/components/Top/topBar.vue
+++ src/components/Top/topBar.vue
@@ -5,7 +5,7 @@
 
 <template>
     <!-- width 63% -->
-   <div class="rounded-xl h-[4rem] bg-white flex items-center">
+   <div class="rounded-xl h-[4rem] px-3 bg-white flex items-center">
         <div>icon</div>
     </div>
 </template>
--- src/views/home/dashboard.vue
+++ src/views/home/dashboard.vue
@@ -9,7 +9,7 @@ const cardList = [
 
 
 <template>
-    <div class="bg-white rounded-xl overflow-y-auto grid grid-cols-4 gap-2 py-1 pr-2 flex-1">
+    <div class="bg-white p-1 rounded-xl overflow-y-auto grid grid-cols-4 gap-2 py-1 pr-2 flex-1">
         <el-card shadow="hover" v-for="(card, index) in cardList" :key="index" :body-style="{ padding: '20px' }" class="cursor-pointer max-h-[250px]">
             <template #header>
                 <div>

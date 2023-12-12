27ae53f4e35b1c2805b8ec3964e1d851f54e8d79
LiuKun <13655914562@163.com>
2023-12-12 21:58:48 +0800

    ud category list

---
+++ mock/category.ts
@@ -0,0 +1,22 @@
+import { Random } from 'mockjs';
+import { MockMethod } from "vite-plugin-mock"
+
+export default [
+    {
+        url: '/api/category',
+        method: 'get',
+        response: () => {
+            return {
+                code: 200,
+                message: '请求成功',
+                data: new Array(5).fill(0).map((_r, index) => {
+                    return {
+                        id: index,
+                        title: Random.ctitle(),
+                        icon: 'fab fa-algolia',
+                    }
+                })
+            }
+        }
+    },
+] as MockMethod[]

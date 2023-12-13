e024dd9cfd8d6ae5a57a9bed43db31f6316adbf7
LiuKun <13655914562@163.com>
2023-12-13 22:40:51 +0800

    Update sideMenu.vue

--- src/components/Home/sideMenu.vue
+++ src/components/Home/sideMenu.vue
@@ -7,20 +7,22 @@ const changeScreen = () => {
 
 
 <template>
-    <div :class="isScreen ? 'sideMenu' : 'hideSideMenu'" class="h-screen bg-white flex justify-center rounded-xl"
+    <div :class="isScreen ? 'sideMenu' : 'hideSideMenu'" class="h-screen bg-white flex flex-col items-center rounded-xl"
         @click="changeScreen">
         <div class="h-[4rem] flex items-center">
             <i class="fab fa-accusoft p-4 text-3xl"></i>
             <span :class="isScreen ? '' : 'hidden'" class="mr-2">jizhu后台管理</span>
         </div>
-        <div class="menu "></div>
+        <div class="menu ">
+            dwdwdwwd
+        </div>
 
     </div>
 </template>
 
 
 <style lang="scss" scoped>
-.sideMenu { 
+.sideMenu {
     @apply w-[230px];
 }
 </style>

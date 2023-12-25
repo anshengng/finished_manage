ea58ac99cf0b1bef4ab2b318ce629370c8c6453a
LiuKun <13655914562@163.com>
2023-12-25 22:04:14 +0800

    add manage_page

---
+++ src/views/manage/goods_manage.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+
+<template>
+    goods manage
+</template>
+
+
+<style lang="scss" scoped></style>
---
+++ src/views/manage/store_manage.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+
+<template>
+    store manage
+</template>
+
+
+<style lang="scss" scoped></style>

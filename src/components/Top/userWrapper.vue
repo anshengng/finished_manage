d7bfe700bd7fd8a0abdc14583cfabb869ebe98f7
LiuKun <13655914562@163.com>
2023-12-27 22:15:28 +0800

    test userinfo?

--- src/components/Top/userWrapper.vue
+++ src/components/Top/userWrapper.vue
@@ -6,7 +6,11 @@ const props = defineProps<{ userinfo: any }>()
 
 
 <template>
-    <div>{{ props.userinfo.username }}</div>
+    <div v-if="props.userinfo?.username">{{ props.userinfo.username }}</div>
+    <div v-else class="">
+        <button class="p-2 border-[1px] border-slate-400 rounded-lg mr-2 h-8 leading-3 cursor-pointer hover:bg-slate-600 hover:text-white">登陆</button>
+        <button class="p-2 border-[1px] border-slate-400 rounded-lg h-8 leading-3 cursor-pointer hover:bg-slate-600 hover:text-white">注册</button>
+    </div>
 </template>
 
 
--- src/store/userStore.ts
+++ src/store/userStore.ts
@@ -3,7 +3,7 @@ import { defineStore } from "pinia";
 
 export default defineStore('user', {
     state: () => ({
-        userInfo: {} as User
+        userInfo: {}
     }),
     actions: {
         async getUserInfo() {

43cfb38d3eae4242419265a97657e8dc5ac8685e
LiuKun <13655914562@163.com>
2023-12-28 22:20:09 +0800

    Update userWrapper.vue

--- src/components/Top/userWrapper.vue
+++ src/components/Top/userWrapper.vue
@@ -8,8 +8,8 @@ const props = defineProps<{ userInfo: any }>()
 <template>
     <div v-if="props.userInfo?.username">{{ props.userInfo.username }}</div>
     <div v-else class="">
-        <button class="p-2 border-[1px] border-slate-400 rounded-lg mr-2 h-8 leading-3 cursor-pointer hover:bg-slate-600 hover:text-white">登陆</button>
-        <button class="p-2 border-[1px] border-slate-400 rounded-lg h-8 leading-3 cursor-pointer hover:bg-slate-600 hover:text-white">注册</button>
+        <button class="p-2 border-[1px] border-slate-400 rounded-lg mr-2 h-8 leading-3 cursor-pointer hover:bg-slate-600 hover:text-white" @click="$router.push('/login')">登陆</button>
+        <button class="p-2 border-[1px] border-slate-400 rounded-lg h-8 leading-3 cursor-pointer hover:bg-slate-600 hover:text-white" @click="$router.push('/register')">注册</button>
     </div>
 </template>
 
